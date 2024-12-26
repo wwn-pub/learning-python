f3fa2b593f0dcd05cf9642b83daa9d1eb9fd529e
Sonam Vajpayee <saraamitdube@gmail.com>
2024-12-26 22:02:52 +0200

    Dictonary

---
+++ class3/dict.py
@@ -0,0 +1,5 @@
+name = {'First':'Sonam', 'Last': 'Vajpayee', 'Age':36, 'DOB': '27/06/1989'}
+print('Keys:', list(name.keys()))
+print('Values:', list(name.values()))
+for i,j in name.items():
+    print(i,j)

4edbbbd1c23416eb3899661c3e526253476a85b9
Sonam Vajpayee <saraamitdube@gmail.com>
2024-12-28 10:52:13 +0200

    Learned map(lambda)

---
+++ class3/map.py
@@ -0,0 +1,8 @@
+num = (2,3,4,5,6,7)
+list = []
+# print('num_line:', num)
+# four = map(lambda x: 4*x,(num))
+# print('four times:', list(four))
+for val in num:
+    list.append(val*2)
+print(list)
